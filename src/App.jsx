6aec479cb960f9e610468bf023dbb2b715cd0091
Hemanth-konduri <konduri.hemanth.s89@gmail.com>
2025-04-02 14:25:09 +0530

    Deploying completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hemanth Konduri</h1>
+      <h2>konduri.hemanth.s89@kalvium community</h2>
+      <h3>kondurihemanth62@gmail.com</h3>
+      <h2>Pedda Pattapu Palem, Rajupalem village, kothapatnam mandal, prakasam dist, AP, India.</h2>
+    </>
+  )
+}
+
+export default App
